6d4d2854f83cd4da65dcc7000d2aa48d4314b4e0
giovannifranchi <giovanni.franchi95@gmail.com>
2023-03-15 17:25:18 +0100

    Feat: milestone 1 completed

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 const {createApp} = Vue;
 
+class ToDos{
+    constructor(task){
+        this.task = task;
+        this.isDone = true;
+    }
+}
+
+const tasks = ['Go to the Doctor', 'Buy milk', "Go to granpa's house", 'Play Soccer'];
+
+const tasksObj = tasks.map((task)=> new ToDos(task));
+
 createApp({
     data(){
         return{
             pageTitle: 'TodoList',
+            todos: tasksObj,
         }
     }
 }).mount('#app');
